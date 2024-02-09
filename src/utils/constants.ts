0479f044813c20a7de308f0a33ce2296e189c807
sjaras <sjara@mined.academy>
2024-02-09 11:05:38 -0500

    feat(components): add Navbar, Content, Title, Button and Hero compoonents, add constants and config in tailwind

---
+++ src/utils/constants.ts
@@ -0,0 +1,28 @@
+export const styles = {
+    whatsapp : "text-white bg-primary bg-[#59DD19] text-lg py-3 px-4 rounded-full "
+}
+
+export const text = {
+    contact : "Contáctanos",
+    navbar : [
+        {
+        text: "Nosotros",
+        },
+        {
+        text: "Servicios",
+        },
+        {
+        text: "Ubícanos",
+        }
+    ],
+    hero : {
+        title: "Laborum consectetur nisi pariatur",
+        content : "Ullamco incididunt qui nulla nostrud.Commodo duis do est tempor qui amet sit. Cillum ipsum elit veniam quis occaecat amet sunt sunt exercitation reprehenderit ullamco anim irure."
+    }
+}
+
+const phone = "983480623"
+
+export const links = {
+    whatsapp : `https://wa.me/+51${phone}?text=Hola%20quiero%20más%20información`
+}
