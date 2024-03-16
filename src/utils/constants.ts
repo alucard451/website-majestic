c6e9551fb5b03c4fc02fa1e4da905de0f6e5987b
sjaras <sjara@mined.academy>
2024-03-15 23:21:41 -0500

    feat(desing): Redesign in Navbar, Hero and Component Offer

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,11 @@
 export const styles = {
-    whatsapp : "text-white bg-primary bg-[#59DD19] text-lg py-3 px-4 rounded-full "
+    whatsapp : "text-white font-semibold px-8 bg-[#25D366] text-lg py-3 px-4 rounded-3xl",
+    whatsappToR : "text-white font-semibold px-8 bg-gradient-to-r from-[#77E596] to-[#95F365] text-lg py-3 px-4 rounded-3xl",
+    textHero: "font-bold bg-gradient-to-r from-[#1FA5FF] to-[#0AEAF5] text-transparent bg-clip-text text-6xl py-2",
+    contentHero: "text-lg font-bold text-[#242424] py-2",
+    textAboutUs: "font-bold bg-gradient-to-r from-[#37E472] to-[#00F4C9] text-transparent bg-clip-text text-6xl py-2 text-end",
+    contentAboutUs: "text-lg font-bold text-[#242424] py-2 text-end"
+
 }
 
 export const text = {
@@ -16,8 +22,12 @@ export const text = {
         }
     ],
     hero : {
-        title: "Laborum consectetur nisi pariatur",
-        content : "Ullamco incididunt qui nulla nostrud.Commodo duis do est tempor qui amet sit. Cillum ipsum elit veniam quis occaecat amet sunt sunt exercitation reprehenderit ullamco anim irure."
+        title: "Que tus diseños creen una historia aún mejor.",
+        content : "Transformamos tus ideas en impresiones memorables."
+    },
+    aboutUs: {
+        title: "Sobre Nosotros",
+        content: "Llevamos la impresión a nuevas alturas. Con dedicación a la excelencia y tecnología de vanguardia, ofrecemos soluciones de impresión de alta calidad que impulsan la visión de nuestros clientes.Desde diseños creativos hasta ejecución impecable, somos su socio de confianza en el mundo de la impresión."
     }
 }
 
---
+++ src/utils/interfaces.ts
@@ -0,0 +1,9 @@
+export interface ButtonProps {
+    text: string;
+    link: string;
+    style: ButtonStyles;
+}
+
+export interface ButtonStyles {
+    whatsapp: "text-white bg-primary bg-[#59DD19] text-lg py-3 px-4 rounded-full "
+}
