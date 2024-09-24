552f3038b0d6229da2ab7ccc85129c29ff50a658
sjaras <sjara@mined.academy>
2024-09-23 23:46:09 -0500

    feat: 🎸 add new sectioons

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,39 +1,63 @@
 export const styles = {
-    whatsapp : "text-white font-semibold px-8 bg-[#25D366] text-lg py-3 px-4 rounded-3xl",
-    whatsappToR : "text-white font-semibold px-8 bg-gradient-to-r from-[#77E596] to-[#95F365] text-lg py-3 px-4 rounded-3xl",
-    textHero: "lg:font-bold font-semibold bg-gradient-to-r from-[#1FA5FF] to-[#0AEAF5] text-transparent bg-clip-text lg:text-6xl text-5xl py-2 text-center lg:text-start",
+    whatsapp: "text-white font-semibold px-8 bg-gradient-to-r from-[#77E596] to-[#95F365] text-lg py-3 px-4 rounded-3xl",
+    whatsappToR: "text-white font-semibold px-8 bg-gradient-to-r from-[#77E596] to-[#95F365] text-lg py-3 px-4 rounded-3xl",
+    textHero: "lg:font-bold font-semibold bg-gradient-to-r from-[#FF65CC] to-[#BD5DF5] text-transparent bg-clip-text lg:text-6xl text-5xl py-2 text-center lg:text-start",
+    textLocation: "lg:font-bold font-semibold bg-gradient-to-r from-[#1FA5FF] to-[#0AEAF5] text-transparent bg-clip-text lg:text-6xl text-5xl py-2 text-center lg:text-start",
     contentHero: "lg:text-lg text-sm lg:font-bold font-medium text-[#242424] py-2 text-center lg:text-start",
-    textAboutUs: "lg:font-bold font-semibold bg-gradient-to-r from-[#37E472] to-[#00F4C9] text-transparent bg-clip-text lg:text-6xl text-5xl py-2 lg:text-end text-center",
-    contentAboutUs: "lg:text-lg text-sm lg:font-bold font-medium text-[#242424] py-2 lg:text-end text-center",
-    socialText : "text-center font-semibold bg-gradient-to-t from-[#00D4FF] to-[#206DE5] text-transparent bg-clip-text lg:text-3xl text-2xl py-2"
+    textAboutUs: "lg:font-bold font-semibold bg-gradient-to-r from-[#37E472] to-[#00F4C9] text-transparent bg-clip-text lg:text-6xl text-5xl py-2 lg:text-start text-center",
+    contentAboutUs: "lg:text-lg text-sm lg:font-bold font-medium text-[#242424] py-2 lg:text-start text-center",
+    socialText: "text-center font-semibold bg-gradient-to-t from-[#00D4FF] to-[#206DE5] text-transparent bg-clip-text lg:text-3xl text-2xl py-2",
+    textServices: "lg:font-bold font-semibold bg-gradient-to-r from-[#FFA31A] to-[#F2D518] text-transparent bg-clip-text lg:text-6xl text-5xl py-2 ",
+    contentServices: "lg:text-lg text-sm lg:font-bold font-medium text-[#242424] py-2  text-center",
 
-}
+};
 
 export const text = {
-    contact : "Contáctanos",
-    navbar : [
+    contact: "Contáctanos",
+    navbar: [
         {
-        text: "Nosotros",
+            text: "Nosotros",
         },
         {
-        text: "Servicios",
+            text: "Servicios",
         },
         {
-        text: "Ubícanos",
+            text: "Ubícanos",
         }
     ],
-    hero : {
+    hero: {
         title: "Que tus diseños creen una historia aún mejor.",
-        content : "Transformamos tus ideas en impresiones memorables."
+        content: "Transformamos tus ideas en impresiones memorables."
     },
     aboutUs: {
         title: "Sobre Nosotros",
-        content: "Llevamos la impresión a nuevas alturas. Con dedicación a la excelencia y tecnología de vanguardia, ofrecemos soluciones de impresión de alta calidad que impulsan la visión de nuestros clientes.Desde diseños creativos hasta ejecución impecable, somos su socio de confianza en el mundo de la impresión."
+        content: "Llevamos la impresión a nuevas alturas. Ofrecemos soluciones de impresión de alta calidad a precios competitivos, respaldadas por tecnología de vanguardia y un equipo comprometido."
+    },
+    ourServices: {
+        title: "Nuestros servicios",
+        content: {
+            first: "Estos son algunos de los productos y servicios que ofrecemos.",
+            second: "Desde elegantes tarjetas hasta impresionantes folletos, cada imagen cuenta una historia de creatividad y profesionalismo."
+        }
+    },
+    location: {
+        title: "Nos encantaría conocerte.",
+        content: {
+            main: "Acércate a nuestra imprenta, un espacio donde la creatividad y la calidad se unen. Desde diseños personalizados hasta impresiones en masa, estamos comprometidos a llevar tus ideas al papel con precisión y atención al detalle.",
+            place: "",
+            phone: "+51 983 480 623",
+            operatingHours: "",
+            valueOffering: {
+                price: "Precios económicos",
+                custom: "Personalización",
+                quality: "Calidad"
+            }
+        }
     }
-}
+};
 
-const phone = "983480623"
+const phone = "983480623";
 
 export const links = {
-    whatsapp : `https://wa.me/+51${phone}?text=Hola%20quiero%20más%20información`
-}
+    whatsapp: `https://wa.me/+51${phone}?text=Hola%20quiero%20más%20información`
+};
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,10 +7,11 @@ export default {
         primary: "#5D5A88",
         secondary: "#9795B5",
         button: "#54DD12",
+        "border-gradient": "linear-gradient(to right, #4dc0b5, #3490dc)",
       },
       dropShadow: {
-        'out': '0 2px 10px   rgba(0, 0, 0, 0.15)'
-      }
+        out: "0 2px 10px   rgba(0, 0, 0, 0.15)",
+      },
     },
   },
   plugins: [],
